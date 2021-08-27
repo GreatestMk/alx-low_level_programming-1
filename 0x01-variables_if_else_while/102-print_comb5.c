75b68d2aa85bcd2e63febace6382ea15849334ae
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-08-27 19:24:32 +0200

    initial commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-  int i, c;
+int i, c;
 
-  for (i = 0; i < 99; i++)
-    {
-      for (c = i + 1; c < 100; c++)
-	{
-	  putchar(i / 10 + '0');
-	  putchar(i % 10 + '0');
-	  putchar(' ');
-	  putchar(c / 10 + '0');
-	  putchar(c % 10 + '0');
-	  if (!(i == 98 && c == 99))
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+for (i = 0; i < 99; i++)
+{
+for (c = i + 1; c < 100; c++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(c / 10 + '0');
+putchar(c % 10 + '0');
+if (!(i == 98 && c == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }

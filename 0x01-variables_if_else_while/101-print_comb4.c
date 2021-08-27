fac84dde656dfcf3fe4e97df61dd47ce2b1473c3
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-08-27 19:22:47 +0200

    initial update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,35 +6,35 @@
  */
 int main(void)
 {
-  int i, j, k;
+int i, j, k;
 
 
-  for (i = 48; i < 58; i++)
-    {
+for (i = 48; i < 58; i++)
+{
 
-      for (j = 48; j < 58; j++)
-	{
+for (j = 48; j < 58; j++)
+{
 
-	  for (k = 48; k < 58; k++)
-	    {
-	      if (k <= j)
-		continue;
-	      if (j <= i)
-		continue;
-	      putchar(i);
-	      putchar(j);
-	      putchar(k);
-	      if (i == 55 && j == 56 && k == 57)
-		{
-		}
-	      else
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+for (k = 48; k < 58; k++)
+{
+if (k <= j)
+continue;
+if (j <= i)
+continue;
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == 55 && j == 56 && k == 57)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }

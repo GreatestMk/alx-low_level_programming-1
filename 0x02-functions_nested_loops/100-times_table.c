677900ff1887d99f8cef7bcce416a56379d10f9b
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-08-31 21:49:12 +0200

    initial commmit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,48 @@
+/*
+ * File: 100-times_table.c
+ * Auth: Thulani Moko
+ */
+
 #include "holberton.h"
+
 /**
- * print_times_table - prints the multiplicaiton times table
- * @n: takes in the what times table user wants, type int
- * Prints only digts and uses _putchar function
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-  int c, r, num, tens;
+int num, mult, prod;
 
-  if (!(n >= 0 && n < 15))
-    return;
-  for (r = 0; r <= n; r++)
-    {
-      for (c = 0; c <= n ; c++)
-	{
-	  num = r * c;
-	  tens = num / 10;
-	  if (tens > 9) /* makes sure tens is a single digit ex. 123/10=12%10=2*/
-	    tens = tens % 10;
-	  if (c == 0) /*Print the first coloum*/
-	    _putchar('0');
-	  else if (tens == 0 && (num / 100) == 0)
-	    {/*print only single digits and so 101 work*/
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar((num % 10) + '0');
-	    }
-	  else if ((num / 100) == 0) /*Print double digits*/
-	    {
-	      _putchar(' ');
-	      _putchar(tens + '0');
-	      _putchar((num % 10) + '0');
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
 
-	    }
-	  else /*print everything else*/
-	    {
-	      _putchar((num / 100) + '0');
-	      _putchar(tens + '0');
-	      _putchar((num % 10) + '0');
-	    }
-	  if (c != n)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	} _putchar('\n');
-    }
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+
+prod = num * mult;
+
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
